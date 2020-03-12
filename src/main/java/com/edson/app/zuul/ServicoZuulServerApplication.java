7b32bd5c8bacbd43ca081437ce6278c226e43ab1
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-12 08:51:02 -0300

    Start project

---
+++ src/main/java/com/edson/app/zuul/ServicoZuulServerApplication.java
@@ -0,0 +1,13 @@
+package com.edson.app.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoZuulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoZuulServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edson/app/zuul/ServicoZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edson.app.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
